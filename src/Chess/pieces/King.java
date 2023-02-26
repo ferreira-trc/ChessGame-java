eca33689b0e185bd8b0aeb5358cfc29752e970e2
Tiago Ferreira <tiagoferreira977@gmail.com>
2023-02-26 17:11:35 +0000

    Knight

--- src/Chess/pieces/King.java
+++ src/Chess/pieces/King.java
@@ -9,8 +9,7 @@ public class King extends ChessPiece{
 
     public King(Board board, Color color) {
         super(board, color);
-    }
-    
+    }    
 
     @Override
     public String toString() {
